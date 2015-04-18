2cd3136a3524956bbca3d3fcca7ae32c6a047f14
Dmitriy Grechukha <dmitriy.grechukha@gmail.com>
2015-04-18 23:20:51 +0300

    Fixed: Identical project names of different users

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,5 @@ class Project < ActiveRecord::Base
   belongs_to :user
   has_many :tasks, -> { order 'tasks.position' }, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, uniqueness: { case_sensitive: false, scope: :user_id }
 end
